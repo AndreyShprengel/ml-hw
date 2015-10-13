95816119e74551c76d0a1350123512ec303c235b
Andrey Shprengel <andrey.shprengel@colorado.edu>
2015-10-13 11:57:09 -0600

    done svm

--- svm/csvm.py
+++ svm/csvm.py
@@ -2,6 +2,24 @@ from sklearn.svm import SVC
 import argparse
 import numpy
 import pprint
+def show(image):
+    """
+    Render a given numpy.uint8 2D array of pixel data.
+    """
+    from matplotlib import pyplot
+    import matplotlib as mpl
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1,1,1)
+    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
+    imgplot.set_interpolation('nearest')
+    ax.xaxis.set_ticks_position('top')
+    ax.yaxis.set_ticks_position('left')
+    pyplot.show()
+    
+def frange(x, y, jump):
+  while x < y:
+    yield x
+    x += jump
 class Numbers:
     """
     Class to store MNIST data
@@ -35,24 +53,56 @@ if __name__ == "__main__":
     data = Numbers("../data/mnist.pkl.gz")
     i = 0
     data.train_y = data.train_y[:args.limit]
-    data.train_x = data.train_x[:args.limit]
-    print len(data.train_y)
+    data.train_x = data.train_x[:args.limit] 
+    
+    print "lenx " + str(len(data.train_x))
     
     x = numpy.nditer(data.train_y,flags=['f_index'])
     badI = list()
     while not x.finished:
-        print x.index
         if not (x[0] == 3 or x[0] ==8):
-            print x[0]
             badI.append(x.index)
         x.iternext()
     
     data.train_y = numpy.delete(data.train_y,badI)
-    data.train_x = numpy.delete(data.train_x,badI)
-    print data.train_y
+    data.train_x = numpy.delete(data.train_x,badI,0)
+    print "leny " + str(len(data.test_y))
+    x = numpy.nditer(data.test_y,flags=['f_index'])
+    badI = list()
+    while not x.finished:
+        if not (x[0] == 3 or x[0] ==8):
 
+            badI.append(x.index)
+        x.iternext()
+    print "leny " + str(len(data.test_y))
 
-        
+    data.test_y = numpy.delete(data.test_y,badI)
+    data.test_x = numpy.delete(data.test_x,badI,0)
+    pprint.pprint(data.train_x)
+    pprint.pprint(data.train_y)
+
+    f = open("data1.txt", "a")
+    
+    clf = SVC(C = 11, kernel = 'linear') 
+    clf.fit(data.train_x, data.train_y)
+    numRight = 0 
+    for i in range(len(data.test_x)):
+        if clf.predict(data.test_x[i]) == data.test_y[i]:
+           numRight += 1
+    acc = float(numRight)/len(data.test_x)
+    print  "accuracy: " + str(acc)
+    sv = clf.support_vectors_
+    print "numsupvec" + str(len(sv))
+    image_arr =  clf.support_vectors_[1] #the front parts of it are 3s, the rear ones are 8s 
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    plt.imshow(numpy.reshape(image_arr, (28, 28)), cmap = cm.Greys_r)
+    plt.show() 
+       # f. write('linear' + ", " +str(acc) + "\n")
+    image_arr =  clf.support_vectors_[len(clf.support_vectors_)-1] #the front parts of it are 3s, the rear ones are 8s 
+    plt.imshow(numpy.reshape(image_arr, (28, 28)), cmap = cm.Greys_r)
+    plt.show() 
+
 
 
 
